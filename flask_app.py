259bb946a272a876ecd2eaa4a1637ca902739d90
Navid Bin Ahmed <45857107+NavidBinAhmed@users.noreply.github.com>
2024-11-11 21:33:36 +0600

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -28,7 +28,7 @@ def predict():
     final_input = scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output = regmodel.predict(final_input)[0]
-    return render_template("home.html", prediction_text="The Tentative Price for House is {}".format(output))
+    return render_template("home.html", prediction_text="The Tentative Price for House is US$ {}K".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
